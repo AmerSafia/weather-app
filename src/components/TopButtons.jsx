55c59b4cbfe905b13990b7d37ea0b1aea54fa0f0
Amersafia <amer.safia321@gmail.com>
2022-09-05 15:03:48 +0300

    add api weather

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 
-const TopButtons = () => {
+function TopButtons({ setQuery }) {
   const cities = [
-    { id: 1, title: "Amman" },
-    { id: 2, title: "AlQuds" },
-    { id: 3, title: "doha" },
-    { id: 4, title: "Cairo" },
-    { id: 5, title: "Tunisia" },
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "Sydney",
+    },
+    {
+      id: 3,
+      title: "Tokyo",
+    },
+    {
+      id: 4,
+      title: "Toronto",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
   ];
+
   return (
-    <div className="flex items-center justify-around my-4">
+    <div className="flex items-center justify-around my-6">
       {cities.map((city) => (
-        <button key={city.id}  className=" text-white text-lg font-medium  ">{city.title}</button>
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
       ))}
     </div>
   );
-};
+}
 
 export default TopButtons;
